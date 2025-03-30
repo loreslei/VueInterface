7756049527d04b852761ce9e0354ccc55fb8635c
Lorenna Aguiar Nunes <133262137+loreslei@users.noreply.github.com>
2025-03-30 10:33:12 -0300

    I didn't understand

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <header class="header">
+        Header
+    </header>
+</template>
+
+<script>
+    export default{
+    }
+</script>
+
+<style>
+.header{
+    color: blue;
+    background-color: pink;
+}
+</style>
